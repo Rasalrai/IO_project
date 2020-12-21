3b9f65701c8bcfd46075a36ed87356df1e08c879
Grzegorz Szot <48454901+gszot@users.noreply.github.com>
2020-12-21 23:53:37 +0100

    chooser remover completed

--- src/main/java/pl/put/poznan/JSONtools/logic/JSONCleaner.java
+++ src/main/java/pl/put/poznan/JSONtools/logic/JSONCleaner.java
@@ -1,5 +1,7 @@
 package pl.put.poznan.JSONtools.logic;
 
+import java.util.Collection;
+
 /**
  * JSONCleaner.java
  * Interface implementing Decorator Design Pattern
@@ -11,7 +13,8 @@ public interface JSONCleaner {
      * Method for processing JSON String
      *
      * @param JSONFile JSON String
+     * @param fieldNames Field Names Collection
      * @return processed JSON String
      */
-    public String clean(String JSONFile);
+    public String clean(String JSONFile, Collection<String> fieldNames);
 }
--- src/main/java/pl/put/poznan/JSONtools/logic/JSONRemover.java
+++ src/main/java/pl/put/poznan/JSONtools/logic/JSONRemover.java
@@ -1,9 +1,42 @@
 package pl.put.poznan.JSONtools.logic;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.util.Collection;
+
+/**
+ * JSONRemover.java
+ * Class implementing Decorator Design Pattern.
+ * Decorator removing fields from JSON String
+ *
+ */
 public class JSONRemover implements JSONCleaner{
 
+    /**
+     * Method for processing JSON String
+     *
+     * @param JSONFile JSON String
+     * @param fieldNames Field Names Collection
+     * @return original JSON String with given fields removed
+     */
     @Override
-    public String clean(String JSONFile){
-        return JSONFile;
+    public String clean(String JSONFile, Collection<String> fieldNames){
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode objectNode = null;
+        try {
+            objectNode = objectMapper.readValue(JSONFile, ObjectNode.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return JSONFile;
+        }
+        try {
+            objectNode.remove(fieldNames);
+        } catch (Exception e){
+            e.printStackTrace();
+            return JSONFile;
+        }
+        return objectNode.toString();
     }
 }
