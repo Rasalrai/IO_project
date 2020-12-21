86d024596727656c3d10b0774d5fdc15a3b4a443
Grzegorz Szot <48454901+gszot@users.noreply.github.com>
2020-12-21 23:53:37 +0100

    chooser remover completed

--- src/main/java/pl/put/poznan/JSONtools/logic/JSONChooser.java
+++ src/main/java/pl/put/poznan/JSONtools/logic/JSONChooser.java
@@ -1,9 +1,42 @@
 package pl.put.poznan.JSONtools.logic;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.util.Collection;
+
+/**
+ * JSONChooser.java
+ * Class implementing Decorator Design Pattern.
+ * Decorator retaining fields from JSON String
+ *
+ */
 public class JSONChooser implements JSONCleaner{
 
+    /**
+     * Method for processing JSON String
+     *
+     * @param JSONFile JSON String
+     * @param fieldNames Field Names Collection
+     * @return original JSON String with given fields retained
+     */
     @Override
-    public String clean(String JSONFile){
-        return JSONFile;
+    public String clean(String JSONFile, Collection<String> fieldNames){
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode objectNode = null;
+        try {
+            objectNode = objectMapper.readValue(JSONFile, ObjectNode.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return JSONFile;
+        }
+        try {
+            objectNode.retain(fieldNames);
+        } catch (Exception e){
+            e.printStackTrace();
+            return JSONFile;
+        }
+        return objectNode.toString();
     }
 }
