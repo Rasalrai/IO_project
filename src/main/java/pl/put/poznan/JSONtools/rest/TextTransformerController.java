f3d2acf49a9a64c210a8035185c314ae68875b17
Julia <jula.stachowiak@gmail.com>
2020-12-21 22:09:00 +0100

    REST API: compare and format JSONs, with help available

--- src/main/java/pl/put/poznan/JSONtools/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/JSONtools/rest/TextTransformerController.java
@@ -1,5 +1,4 @@
 package pl.put.poznan.JSONtools.rest;
-import com.fasterxml.jackson.databind.ser.Serializers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -7,48 +6,63 @@ import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.JSONtools.logic.*;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Base64;
 
 
 @RestController
-@RequestMapping("/{text}")
 public class TextTransformerController {
+
+    private String decodeParam(String param) {
+        return new String(Base64.getDecoder().decode(param), StandardCharsets.UTF_8);
+    }
 
     private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
 
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public String get(@PathVariable String text,
-                          @RequestParam(value="f1") String f1,
-                          @RequestParam(value="f2") String f2) {
+    private final JSONMinifier jm = new JSONMinifier();
+    private final JSONPrettifier jp = new JSONPrettifier();
 
-        String j1 = new String(Base64.getDecoder().decode(f1), StandardCharsets.UTF_8);
-        String j2 = new String(Base64.getDecoder().decode(f2), StandardCharsets.UTF_8);
+    @RequestMapping(path = "/help", method = RequestMethod.GET, produces = "application/json")
+    public String get_help() {
 
         // log the parameters
-        logger.debug(text);
+        logger.info("help");
+        return jp.decorate("{\"Available\":{\"POST\":{\"/compare?left=file1&right=file2\":\"compare base64 encoded files\",\"/min\":\"minify the request body\",\"/pretty\":\"formats the request body for human-readability\"},\"GET\":{\"/help\":\"get help; you are here.\"}}}");
+    }
+
+    @RequestMapping(path = "/compare", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String compare(@RequestParam(value="left") String f1,
+                          @RequestParam(value="right") String f2) {
+
+        String j1 = decodeParam(f1);
+        String j2 = decodeParam(f2);
+
+        // log the parameters
+        logger.info("compare");
         logger.debug(j1);
-        logger.debug(j2);
+        logger.debug(j2+"\n\n");
 
         // perform the transformation, you should run your logic here, below is just a silly example
         JSONCompare jc = new JSONCompare(j1, j2);
         return jc.compare();
     }
 
-    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public String post(@PathVariable String text,
-                       @RequestBody String transforms) {
+    @RequestMapping(path = "/min", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String minify(@RequestBody String json) {
 
         // log the parameters
-        logger.debug(text);
-        logger.debug(transforms);
-//        logger.debug(Arrays.toString(transforms));
+        logger.info("minify");
+        logger.debug(json+"\n\n");
 
-        JSONPrettifier jm = new JSONPrettifier();
-        transforms = jm.decorate(transforms);
-        return transforms;
+        return jm.decorate(json);
     }
 
+    @RequestMapping(path = "/pretty", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String prettify(@RequestBody String json) {
+
+        // log the parameters
+        logger.info("prettify");
+        logger.debug(json+"\n\n");
+
+        return jp.decorate(json);
+    }
 }
-
-
